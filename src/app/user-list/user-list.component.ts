c18f49210c6316e839cfaae789e4f87306d30e08
Laurenkad <166012651+Laurenkad@users.noreply.github.com>
2024-11-06 08:54:44 +0200

    Update user-list.component.ts

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -11,9 +11,9 @@ interface Address {
 
 interface User {
   id: number;
-  name: string; // Defaults to "NA" in usage if necessary
-  birthdate: string; // Defaults to "NA" in usage if necessary
-  addresses: Address[]; // Array of Address objects
+  name: string;
+  birthdate: string;
+  addresses: Address[];
 }
 @Component({
   selector: 'user-list',
@@ -23,23 +23,22 @@ interface User {
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent implements OnInit {
-  constructor(private countriesService: CountriesService) {} // קונסטרוקטור של הרכיב, מוזן בשירות CountriesService
-dataSource: User[] = []; // משתנה שאוסף נתונים של משתמשים
+  constructor(private countriesService: CountriesService) {} 
+dataSource: User[] = []; 
 displayedColumns: string[] = ['Id', 'name', 'birthdate', 'addresses']; // העמודות להצגה בטבלה
 
-// משמש לטעינת נתונים 
-// כי זו פונקציה מופעלת כשהקומפוננטה מופעלת לראשונה
-ngOnInit(): void { // מופעל כאשר הרכיב מוכן
-  this.initData(); // מפעיל את פונקציית initData לטעינת הנתונים
-  this.countriesService.refreshtable.subscribe((data) => { // מאזין לשינויים ב-refreshtable
-    if (data) // אם התקבל נתון
-      this.initData(); // טוען מחדש את הנתונים
+
+ngOnInit(): void { 
+  this.initData(); 
+  this.countriesService.refreshtable.subscribe((data) => { 
+    if (data) 
+      this.initData(); 
   });
 }
 
-initData() { // פונקציה לטעינת נתוני משתמשים
-  this.countriesService.getPersons().subscribe((data) => { // מבצע מנוי על קריאת שירות לקבלת משתמשים
-    this.dataSource = data; // מאחסן את הנתונים במשתנה dataSource
+initData() { 
+  this.countriesService.getPersons().subscribe((data) => { 
+    this.dataSource = data; 
   });
 }
 }
